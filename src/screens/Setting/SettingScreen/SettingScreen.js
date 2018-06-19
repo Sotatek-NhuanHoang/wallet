26207c30051547a4efa131b9ca47f35e053dfdbf
nn <nhuan>
2018-06-19 15:11:01 +0700

    Complete SettingScreen

--- src/screens/Setting/SettingScreen/SettingScreen.js
+++ src/screens/Setting/SettingScreen/SettingScreen.js
@@ -1,35 +1,55 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { TouchableWithoutFeedback, View, Text } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 import GlobalLoc from '@components/GlobalLoc';
 import GlobalHeaderTitle from '@components/GlobalHeaderTitle';
 import GlobalContainer from '@components/GlobalContainer';
-import GlobalButton from '@components/GlobalButton';
+import GlobalHeaderBackButton from '@components/GlobalHeaderBackButton';
+import { navigate } from '@utils/NavigationService';
+
+import style from '@styles/screens/Setting/SettingScreen/SettingScreen';
 
 
 export class SettingScreen extends Component {
 
     static navigationOptions = {
+        headerLeft: (
+            <GlobalHeaderBackButton />
+        ),
         headerTitle: (
             <GlobalHeaderTitle>
-                <GlobalLoc locKey="SettingScreen.title" />
+                <GlobalLoc locKey="Setting.SettingScreen.title" />
             </GlobalHeaderTitle>
         ),
     };
 
 
+    constructor(props) {
+        super(props);
+        this.onLanguageSettingClicked = this.onLanguageSettingClicked.bind(this);
+    }
+
+
+    onLanguageSettingClicked() {
+        navigate('LanguageScreen');
+    }
+
+
     render() {
         return (
-            <GlobalContainer>
-                <GlobalButton
-                    title="HomeScreen"
-                    onPress={() => this.props.navigation.navigate('HomeScreen')}
-                />
+            <GlobalContainer style={ style.container }>
+                 <TouchableWithoutFeedback onPress={ this.onLanguageSettingClicked }>
+                    <View style={ style.settingContainer }>
+                        <View style={{ flex: 1 }}>
+                            <GlobalLoc locKey="Setting.SettingScreen.language" style={ style.setting_Text } />
+                        </View>
 
-                <GlobalButton
-                    title="LanguageScreen"
-                    onPress={() => this.props.navigation.navigate('LanguageScreen')}
-                />
+                        <View>
+                            <Icon name="angle-right" style={ style.setting_Icon } />
+                        </View>
+                    </View>
+                </TouchableWithoutFeedback>
             </GlobalContainer>
         );
     }
