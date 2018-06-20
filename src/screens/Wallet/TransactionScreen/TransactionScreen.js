f83149225b57bbe6ad019c9366ca932a66bf7ae4
Sotatek-ninhtran <ninh.tran@sotatek.com>
2018-06-20 15:57:31 +0700

    gui

--- src/screens/Wallet/TransactionScreen/TransactionScreen.js
+++ src/screens/Wallet/TransactionScreen/TransactionScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, FlatList } from 'react-native';
+import { Text, View, SectionList, TouchableWithoutFeedback } from 'react-native';
 import { connect } from 'react-redux';
 
 import GlobalLoc from '@components/GlobalLoc';
@@ -24,10 +24,123 @@ export class TransactionScreen extends Component {
         ),
     };
 
+    constructor(props) {
+        super(props);
+        this.renderItem = this.renderItem.bind(this);
+        this.renderSectionHeader = this.renderSectionHeader.bind(this);
+    }
+
+    onPressItem() {
+    // open web
+    }
+
+    renderItem({ item, sectionID }) {
+        return (
+            <TouchableWithoutFeedback
+                key={sectionID}
+                onPress={this.onPressItem}
+                style={style.contain}
+            >
+                <View style={style.itemContain}>
+                    <View style={style.itemHeader}>
+                        <Text style={style.itemStatus}>{item.status_mesg}</Text>
+                        <Text style={[style.itemMount, item.status_id === '0' ? { color: 'red' } : { color: 'green' }]}>{item.mount}</Text>
+                    </View>
+                    <Text style={style.itemAddress}>{item.address}</Text>
+                </View>
+            </TouchableWithoutFeedback>
+        );
+    }
+
+    renderSectionHeader({section}) {
+        return (
+            <View style={style.itemHeader}>
+                <View style={style.seperator} />
+                <Text style={style.dateTrans}>{section.title}</Text>
+                <View style={style.seperator} />
+            </View>
+        );
+    }
 
     render() {
         const { selectedCoin } = this.props;
-
+        let fakeData = [
+            {
+                title: '2018/05/',
+                data: [
+                    {
+                        status_id: '0',
+                        status_mesg: 'Đã gửi tiền đi',
+                        mount: '-100.097',
+                        address: 'x0xxxxxxxxxxxxxxxxxxxxxx'
+                    },
+                    {
+                        status_id: '1',
+                        status_mesg: 'Đã nhận được',
+                        mount: '+9.765',
+                        address: 'x0xxxxxxxxxxxxxxxxxxxxxx'
+                    },
+                ]
+            },
+            {
+                title: '2018/06/',
+                data: [
+                    {
+                        status_id: '0',
+                        status_mesg: 'Đã gửi tiền đi',
+                        mount: '-100.097',
+                        address: 'x0xxxxxxxxxxxxxxxxxxxxxx'
+                    },
+                    {
+                        status_id: '0',
+                        status_mesg: 'Đã gửi tiền đi',
+                        mount: '-100.097',
+                        address: 'x0xxxxxxxxxxxxxxxxxxxxxx'
+                    },
+                    {
+                        status_id: '1',
+                        status_mesg: 'Đã nhận được',
+                        mount: '+8.0091',
+                        address: 'x0xxxxxxxxxxxxxxxxxxxxxx'
+                    },
+                    {
+                        status_id: '0',
+                        status_mesg: 'Đã gửi tiền đi',
+                        mount: '-100.097',
+                        address: 'x0xxxxxxxxxxxxxxxxxxxxxx'
+                    },
+                ]
+            },
+            {
+                title: '2018/07/',
+                data: [
+                    {
+                        status_id: '0',
+                        status_mesg: 'Đã gửi tiền đi',
+                        mount: '-100.097',
+                        address: 'x0xxxxxxxxxxxxxxxxxxxxxx'
+                    },
+                    {
+                        status_id: '0',
+                        status_mesg: 'Đã gửi tiền đi',
+                        mount: '-100.097',
+                        address: 'x0xxxxxxxxxxxxxxxxxxxxxx'
+                    },
+                    {
+                        status_id: '1',
+                        status_mesg: 'Đã nhận được',
+                        mount: '+8.0091',
+                        address: 'x0xxxxxxxxxxxxxxxxxxxxxx'
+                    },
+                    {
+                        status_id: '0',
+                        status_mesg: 'Đã gửi tiền đi',
+                        mount: '-100.097',
+                        address: 'x0xxxxxxxxxxxxxxxxxxxxxx'
+                    },
+                ]
+            }
+        ];
         return (
             <GlobalContainer>
                 {/* Icon and coin name */}
@@ -35,8 +148,12 @@ export class TransactionScreen extends Component {
                     <GlobalCoinIcon coin={selectedCoin.coin} size="large" />
                     <Text style={style.coinName}>{selectedCoin.coinName}</Text>
                 </View>
-                  {/* list transaction*/}
-                <FlatList></FlatList>
+                {/* list transaction*/}
+                <SectionList
+                    renderItem={this.renderItem}
+                    renderSectionHeader={this.renderSectionHeader}
+                    sections={fakeData}
+                />
             </GlobalContainer>
         );
     }
--- src/styles/screens/Wallet/TransactionScreen/TransactionScreen.js
+++ src/styles/screens/Wallet/TransactionScreen/TransactionScreen.js
@@ -2,15 +2,55 @@ import { ScaledSheet } from '@react-native-size-matters';
 import { Colors, FontSizes, Sizes } from '../../../variables';
 
 export default ScaledSheet.create({
+  contain:{
+    flex:1
+  },
+  itemContain:{
+    flex:1, 
+    paddingTop:'10@ms', 
+    paddingBottom:'10@ms'
+  },
+  itemHeader:{
+    flex: 1, 
+    flexDirection: 'row' ,
+    alignItems:'center',
+  },
+  itemStatus:{
+    flex: 1, 
+    textAlign: 'left',
+    fontSize: FontSizes.p,
+  },
+  itemMount:{
+    flex: 1, 
+    textAlign: 'right',
+    fontSize: FontSizes.p
+  },
+  itemAddress:{
+    fontSize: FontSizes.p,
+  },
+  seperator:{
+     flex: 1, 
+     height: 1, 
+     backgroundColor: Colors.black,
+     alignItems:'center',
+  },
+  dateTrans:{
+    flex: 1, 
+    flexWrap: 'wrap',
+    fontSize: FontSizes.p,
+    textAlign:'center',
+  },
   coinContainer: {
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
-    marginBottom: '70@ms',
+    marginTop:  '10@ms',
+    marginBottom:  '35@ms',
   },
   coinName: {
     fontSize: FontSizes.h2,
     color: Colors.darkGray,
     marginLeft: Sizes.s4,
-  },
+  }
+ 
 });
