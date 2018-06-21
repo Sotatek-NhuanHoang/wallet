99aed927bfd45dacd2edc900649709e6a99f05f0
nn <nhuan>
2018-06-21 15:38:32 +0700

    Refactor mock data

--- src/screens/Wallet/WalletScreen/WalletScreen.js
+++ src/screens/Wallet/WalletScreen/WalletScreen.js
@@ -17,7 +17,7 @@ import style from '@styles/screens/Wallet/WalletScreen/WalletScreen';
 const CustomHeaderTitle = connect(
     ({ global }) => ({ selectedCoin: global.selectedCoin, })
 )(
-    ({ selectedCoin }) => <Text>{ selectedCoin.coinName }</Text>
+    ({ selectedCoin }) => <Text>{ selectedCoin.name }</Text>
 );
 
 export class WalletScreen extends Component {
@@ -47,14 +47,14 @@ export class WalletScreen extends Component {
     }
 
     render() {
-        const { selectedCoin } = this.props;
+        const { selectedCoin, locale } = this.props;
 
         return (
             <GlobalContainer>
                 {/* Icon and coin name */}
                 <View style={ style.coinContainer }>
-                    <GlobalCoinIcon coin={ selectedCoin.coin } size="large" />
-                    <Text style={ style.coinName }>{ selectedCoin.coinName }</Text>
+                    <GlobalCoinIcon coin={ selectedCoin.symbol } size="large" />
+                    <Text style={ style.coinName }>{ selectedCoin.name }</Text>
                 </View>
 
                 {/* Wallet info */}
@@ -62,18 +62,19 @@ export class WalletScreen extends Component {
                     {/* Wallet balance */}
                     <View style={ style.walletBalanceContainer }>
                         <Text style={ style.walletBalance_Text }>{ selectedCoin.balance }</Text>
-                        <Text style={ style.walletBalance_Coin }>{ currenyFormatFilter(selectedCoin.coin) }</Text>
+                        <Text style={ style.walletBalance_Coin }>{ currenyFormatFilter(selectedCoin.symbol) }</Text>
                     </View>
 
-                    <Text style={ style.coinPrice }>{ coinPriceFormatFilter(selectedCoin.price) }</Text>
+                    {locale === 'ja' && <Text style={ style.coinPrice }>{ coinPriceFormatFilter(selectedCoin.price_jpy) }</Text>}
+                    {locale !== 'ja' && <Text style={ style.coinPrice }>{ coinPriceFormatFilter(selectedCoin.price_usd) }</Text>}
                     <Text style={ style.currency }>
                         <GlobalLoc locKey="Wallet.WalletScreen.currency" />
                     </Text>
 
                     {/* Change value */}
-                    {selectedCoin.change > 0 && <Text style={ [style.walletChange, style.textGreen ] }>+{ selectedCoin.change }%</Text>}
-                    {selectedCoin.change === 0 && <Text style={ style.walletChange }>{ selectedCoin.change }%</Text>}
-                    {selectedCoin.change < 0 && <Text style={ [style.walletChange, style.textRed ] }>{ selectedCoin.change }%</Text>}
+                    {selectedCoin.percent_change_24h > 0 && <Text style={ [style.walletChange, style.textGreen ] }>+{ selectedCoin.percent_change_24h }%</Text>}
+                    {selectedCoin.percent_change_24h === 0 && <Text style={ style.walletChange }>{ selectedCoin.percent_change_24h }%</Text>}
+                    {selectedCoin.percent_change_24h < 0 && <Text style={ [style.walletChange, style.textRed ] }>{ selectedCoin.percent_change_24h }%</Text>}
                 </View>
 
                 {/* Action button */}
@@ -96,8 +97,9 @@ export class WalletScreen extends Component {
 }
 
 
-const mapStateToProps = ({ global, walletInitialSetting }) => ({
+const mapStateToProps = ({ global, walletInitialSetting, i18n }) => ({
     selectedCoin: global.selectedCoin,
+    locale: i18n.locale,
 });
 
 export default connect(mapStateToProps)(WalletScreen);
