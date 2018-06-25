817dd20a5e6327755a59f364a7f472bed84a64e4
nn <nhuan>
2018-06-25 15:03:11 +0700

    Update translation for WithdrawConfirmScreen

--- src/routes.js
+++ src/routes.js
@@ -99,7 +99,7 @@ export const Routes = createStackNavigator(
         WebViewScreen: WebViewScreen,
     },
     {
-        initialRouteName: 'CurrencyListScreen',
+        initialRouteName: 'SplashScreen',
         navigationOptions: ({ navigation }) => {
             const options = { ...defaultNavigationOptions };
 
